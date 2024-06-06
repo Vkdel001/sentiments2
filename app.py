6d40472e79c0b9c9936d782117adab866b0ab895
Vkdel001 <86352647+Vkdel001@users.noreply.github.com>
2024-06-06 16:19:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,8 @@ def index():
         app.logger.info(f'Analyzed text: {text}, Sentiment: {sentiment}')
     return render_template('index.html', sentiment=sentiment)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
